873cbc4c222fc620b9e5e362e9d51493d3be86a7
Peter Babka <159peter951@gmail.com>
2025-07-15 23:52:05 +0200

    progress

--- tests/resources/test_files.py
+++ tests/resources/test_files.py
@@ -1,7 +1,8 @@
+import json
+
 from aresource.manager import ResourceManager
-from aresource.resources.files import (
-    BytesResource,
-)
+from aresource.resources.files import BytesResource, HoconResource, IniResource, JsonResource
+from aresource.resources.files.path import PathResource
 
 
 async def test_bytes() -> None:
@@ -13,3 +14,45 @@ async def test_bytes() -> None:
     async with TestResourceManager() as manager:
         assert isinstance(manager.test, bytes)
         assert b"__all__" in manager.test
+
+
+async def test_hocon() -> None:
+    class TestResourceManager(ResourceManager):
+        """Resource manager containing the ExampleResource for testing."""
+
+        test = HoconResource("tests", "data/test.conf")
+
+    async with TestResourceManager() as manager:
+        assert manager.test == {"key": "value"}
+
+
+async def test_ini() -> None:
+    class TestResourceManager(ResourceManager):
+        """Resource manager containing the ExampleResource for testing."""
+
+        test = IniResource("tests", "data/test.ini")
+
+    async with TestResourceManager() as manager:
+        assert manager.test.sections() == ["forge.example"]
+
+
+async def test_json() -> None:
+    class TestResourceManager(ResourceManager):
+        """Resource manager containing the ExampleResource for testing."""
+
+        test = JsonResource("tests", "data/test.json")
+
+    async with TestResourceManager() as manager:
+        assert manager.test == {"key": "value"}
+
+
+async def test_path() -> None:
+    class TestResourceManager(ResourceManager):
+        """Resource manager containing the ExampleResource for testing."""
+
+        test = PathResource("tests", "data/test.json")
+
+    async with TestResourceManager() as manager:
+        assert manager.test.exists()
+        data = json.loads(manager.test.read_text())
+        assert data == {"key": "value"}
