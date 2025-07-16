d500cdf52a62487b27632bfd89ffc4c18d19281b
Peter Babka <159peter951@gmail.com>
2025-07-16 19:33:30 +0200

    progress

--- tests/resources/test_files.py
+++ tests/resources/test_files.py
@@ -3,6 +3,7 @@ import json
 from aresource.manager import ResourceManager
 from aresource.resources.files import BytesResource, HoconResource, IniResource, JsonResource
 from aresource.resources.files.path import PathResource
+from aresource.resources.files.yaml import YamlResource
 
 
 async def test_bytes() -> None:
@@ -56,3 +57,13 @@ async def test_path() -> None:
         assert manager.test.exists()
         data = json.loads(manager.test.read_text())
         assert data == {"key": "value"}
+
+
+async def test_yaml() -> None:
+    class TestResourceManager(ResourceManager):
+        """Resource manager containing the ExampleResource for testing."""
+
+        test = YamlResource("tests", "data/test.yaml")
+
+    async with TestResourceManager() as manager:
+        assert manager.test == [{"test": "test"}]
